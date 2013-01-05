f92fc855db6ce91b156eeaba38478d44cfcaba3b
Taesoo Kwon <taesoobear@gmail.com>
2013-01-05 19:16:59 +0900

    a

---
+++ install_windows_msysgit.sh
@@ -0,0 +1,7 @@
+mkdir -p ~/bin
+cp gitv ~/bin/
+cp mylib.lua ~/bin/
+mkdir -p /share/vim/vim73/plugin
+cp gitvim.vim /share/vim/vim73/plugin
+chmod +x ~/bin/gitv
+
---
+++ taesoo_vimrc/install_taesoo_only_windows_msysgit.sh
@@ -0,0 +1,11 @@
+cd ..
+sh install_windows_msysgit.sh
+cd taesoo_vimrc
+cp vimrc ~/_vimrc
+currdir=`pwd`
+echo $currdir
+cd /share/vim/vim73
+unzip "$currdir/vimwiki-1-1-1.zip"
+cd /share/vim/vim73/plugin
+mkdir -p /share/vim/vim73/colors
+cp "$currdir/pyte.vim" /share/vim/vim73/colors
