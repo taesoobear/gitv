ccc5d1caa2296bd4f8ae403654f850455d1d6974
Taesoo Kwon <you@example.com>
2013-06-30 22:14:10 +0900

    (no msg)

---
+++ taesoo_vimrc/install_sublime_packages.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.config/sublime-text-2/Packages
+cd ~/.config/sublime-text-2/Packages
+git clone https://github.com/SublimeText/CTags
+git clone https://github.com/sublimehq/Vintage.git "Vintage Dev"
