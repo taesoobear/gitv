626050e678115b23fc909f981679c3f7d1ff5f07
taesoo <taesoo@taesoo-xt-new.(none)>
2013-07-04 10:22:29 +0900

    (no msg)

--- taesoo_vimrc/install_sublime_packages.sh
+++ taesoo_vimrc/install_sublime_packages.sh
@@ -5,6 +5,6 @@ cp *keymap ~/.config/sublime-text-2/Packages/User/
 cd ~/.config/sublime-text-2/Packages
 git clone https://github.com/SublimeText/CTags
 git clone https://github.com/sublimehq/Vintage.git "Vintage Dev"
-#git clone https://github.com/chikatoike/SublimeIBus
+git clone https://github.com/chikatoike/SublimeIBus
 git clone https://github.com/xgenvn/InputHelper.git
 
