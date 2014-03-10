9604a157149ba0a5805a88f0c57cdc9dd54afbf8
taesoo <taesoobear@gmail.com>
2014-03-11 08:17:25 +0900

    (no msg)

--- taesoo_vimrc/install_taesoo_only_windows_cygwin.sh
+++ taesoo_vimrc/install_taesoo_only_windows_cygwin.sh
@@ -2,6 +2,7 @@ cd ..
 sh install.sh
 cd taesoo_vimrc
 cp vimrc ~/.vimrc
+cp vimrc_cygwin_gvim ~/_vimrc
 cp _emacs ~/.emacs
 cp -rf _emacs.d ~/
 mkdir -p ~/.emacs.d
@@ -23,3 +24,5 @@ cp cygwin_win_gvim/_vimrc ~/_vimrc
 cp -rf ~/.vim ~/vimfiles
 cp cygwin_win_gvim/gitvim.vim ~/vimfiles/plugin/
 echo "Note!!! please manually edit ~/.vimrc and remove the line containing clipboard=unnamedplus"
+echo "~/.vimrc is for the cygwin vim."
+echo "~/_vimrc is for windows gvim launched from cygwin. Copy ~/.vim folder to ~/vimfiles after installing bundles using the cygwin vim to use gvim properly."
