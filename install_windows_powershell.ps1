0f28e6691d41f1b4de04133861171ca859cc1847
taesoobear <taesoobear@gmail.com>
2022-09-27 16:20:52 +0900

    gitv installer for powershell!

---
+++ install_windows_powershell.ps1
@@ -0,0 +1,28 @@
+winget install "lua for windows"
+# let's use neovim instead of vim
+winget install Neovim.Neovim
+# install lf file manager (which is a clone of "ranger")
+winget install -e --id GoLang.Go.1.19
+$env:CGO_ENABLED = '0'
+go install -ldflags="-s -w" github.com/gokcehan/lf@late
+
+
+if(!(Test-Path -Path $HOME\bin)) { md $HOME\bin }
+
+copy gitv $HOME\bin\
+copy mylib.lua $HOME\bin\
+copy mylib52.lua $HOME\bin\
+
+
+if(!(Test-Path -Path $HOME\tmp)) { md $HOME\tmp }
+
+
+if(!(Test-Path -Path $HOME\AppData\Local\nvim)) { md $HOME\AppData\Local\nvim }
+copy windows_powershell\init.vim $HOME\AppData\Local\nvim\
+Invoke-WebRequest https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim -OutFile $HOME\AppData\Local\nvim\autoload\plug.vim
+
+if(Test-Path -Path $profile)
+{
+	notepad.exe windows_powershell\powershell_profile
+	notepad.exe $profile
+}
